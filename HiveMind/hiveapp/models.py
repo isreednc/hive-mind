f8dd1836309a13ae9ef5cee20ff7825078b6b786
isreednc <isaacreednc99@gmail.com>
2024-10-30 16:32:28 -0400

    Create hiveapp/urls.py and create initial models

--- HiveMind/hiveapp/models.py
+++ HiveMind/hiveapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+# max character in each string field
+string_max = 200
+
 # Create your models here.
+class User(models.Model):
+    user_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=string_max)
+
+class Team(models.Model):
+    team_id = models.BigAutoField(primary_key=True)
+    users = models.ManyToManyField(User)
+
+class Project(models.Model):
+    team = models.ForeignKey(Team)
+    name = models.CharField(max_length=string_max)
+
