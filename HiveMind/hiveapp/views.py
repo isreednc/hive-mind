4eef6853bcc560a57ecd0d347d8d0cb5a973dc90
isreednc <isaacreednc99@gmail.com>
2024-11-16 16:26:11 -0500

    Create form

--- HiveMind/hiveapp/views.py
+++ HiveMind/hiveapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .forms import LoginForm
 
 # Create your views here.
 def index(request):
     return render(request, 'login.html')
+
+def login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("/hiveapp/")
+    
+    else:
+        form = LoginForm()
+
+    return render(request, "login.html", {"form":form})
