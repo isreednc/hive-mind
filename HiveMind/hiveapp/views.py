9fd53608c92d61f6f1633049354a20702f42f08f
isreednc <isaacreednc99@gmail.com>
2024-11-18 15:51:39 -0500

    Remove duplicate template files

--- HiveMind/hiveapp/views.py
+++ HiveMind/hiveapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'base/homepage.html')
+    return render(request, 'homepage.html')
 
 def projectsPage(request):
     return render(request, 'base/projects.html')
