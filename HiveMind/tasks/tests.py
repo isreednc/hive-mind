7069c5da9bb72a74b9aba6af9e98268f9e7a9e72
isreednc <isaacreednc99@gmail.com>
2024-11-22 01:52:39 -0500

    Create and test task model functionality

--- HiveMind/tasks/tests.py
+++ HiveMind/tasks/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Note, Reply
 
 # Create your tests here.
+class NoteModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="john",
+            password="password"
+        )
+        
+        self.note = Note.objects.create(
+            content="Testing note functionality",
+            user = self.user
+        )
+    
+    def test_note(self):
+        self.assertEqual(self.user.username, "john")
+        self.assertEqual(self.note.content, "Testing note functionality")
+
+    def test_note_string(self):
+        self.assertEqual(str(self.note), "john")
+
+    def test_mark_complete(self):
+        self.note.mark_completed()
+        self.assertTrue(self.note.is_complete)
+    
