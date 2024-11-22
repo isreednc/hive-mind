2076dbad801c5fa9b1d9637ca9e3bf377939c79a
isreednc <isaacreednc99@gmail.com>
2024-11-22 01:52:39 -0500

    Create and test task model functionality

--- HiveMind/tasks/models.py
+++ HiveMind/tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
-# Create your models here.
+# Create your models here
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group = models.ForeignKey
+    content= models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_complete = models.BooleanField(default=False)
+
+    def mark_completed(self):
+        self.is_complete = True
+        self.save()
+
+    def __str__(self):
+        return self.user.username
+
+class Reply(models.Model):
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    content= models.TextField()
