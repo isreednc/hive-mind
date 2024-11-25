40cdcf84db7d9ec1c4ec164c4bb6d5c49d671e8b
isreednc <isaacreednc99@gmail.com>
2024-11-25 17:32:17 -0500

    Implement taskboard frontend into django

--- HiveMind/tasks/utils.py
+++ HiveMind/tasks/utils.py
@@ -1,4 +1,4 @@
 from django.contrib.auth.models import User, Group
 
-def validate_user_group(user, group_id):
-    return user.groups.filter(id=group_id).exists()
+def validate_user_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
--- HiveMind/tasks/views.py
+++ HiveMind/tasks/views.py
@@ -5,21 +5,19 @@ from .models import Note, Reply
 from .utils import *
 
 # Create your views here.
-def task_page(request, group_id):
+def task_page(request, group_name):
     if request.method != "GET":
         pass    # TODO
     
     user = request.user
-    if validate_user_group(user, group_id) == False:
+    if validate_user_group(user, group_name) == False:
         # TODO: implement permission_denied.html
         return render(request, 'permission_denied.html')
     
-    group = Group.objects.filter(id=group_id)
-    group_users = group.user_set.all()
-    notes = Note.objects.filter(Group=group)
+    current_group = Group.objects.get(name=group_name)
+    notes = Note.objects.filter(group=current_group)
 
     return render(request, 'tasks.html', {
-        'group': group,
-        'users': group_users,
+        'group': current_group,
         'notes': notes
     })    
