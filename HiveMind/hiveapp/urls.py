724dbdf38da39b00ecb42ed97dd613a1e0b2f24b
7hangtime <dthrowe1@charlotte.edu>
2024-11-26 13:21:10 -0500

    Login page completion + Django token integration

--- HiveMind/hiveapp/urls.py
+++ HiveMind/hiveapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("projects/", views.projectsPage, name="projects"),
     path('timeline/', views.timelinePage, name='timeline'),
-    path('updates/', views.updatesPage, name='updates')
+    path('updates/', views.updatesPage, name='updates'),
+    path('login_user', views.login_user, name='login'),
 ]
